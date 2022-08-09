a6e6e9c34d74c47fd69cb3fe443fa7ce9141b5fb
Marta Nara <marta.kos1230@gmail.com>
2022-08-09 18:26:52 +0200

    Section 8: Decorators - Decorator Factories

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,16 @@
-// Decorators don't impact end users. They are for other programmers.
-// Decorator is a function. It doesn't have to start with an uppercase but usually they do.
+// Decorator Factory - allows us to customize the decorator function
 
-function Logger(constructor: Function) {
-  console.log('Logging...');
-  console.log(constructor);
+function Logger(logString: string) {
+  return function(constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  }
 }
 
 // Whatever is after the @ mark should point at a decorator (not execute it!)
 // Decorators runs when JS finds the class definition (not when you use the contructor)
 
-@Logger 
+@Logger('LOGGING - PERSON')
 class Person {
   name = 'Max'
 
