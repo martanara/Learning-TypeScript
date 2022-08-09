61f14abbad9c89333fb8d52233952ad634a923c3
Marta Nara <marta.kos1230@gmail.com>
2022-08-09 17:44:22 +0200

    Section 8: Decorators - Class Decorator

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,24 @@
-// Built-in utility: 'Partial'
+// Decorators don't impact end users. They are for other programmers.
+// Decorator is a function. It doesn't have to start with an uppercase but usually they do.
 
-interface CourseGoal {
-  title: string;
-  description: string;
-  completeUntil: Date;
+function Logger(constructor: Function) {
+  console.log('Logging...');
+  console.log(constructor);
 }
 
-function createCourseGoal(
-  title: string, 
-  description: string, 
-  date: Date): CourseGoal {
-  let courseGoal: Partial<CourseGoal> = {};
-  courseGoal.title = title;
-  courseGoal.description = description;
-  courseGoal.completeUntil = date;
-  return courseGoal as CourseGoal;
-} 
+// Whatever is after the @ mark should point at a decorator (not execute it!)
+// Decorators runs when JS finds the class definition (not when you use the contructor)
 
-// Partial - built-in feature of TypeScript - makes all the properties optional (in the end we need to type cast to the proper type)
+@Logger 
+class Person {
+  name = 'Max'
 
-// Built-in utility: 'Readonly' - gives extra strickness
+  constructor() {
+    console.log('Creating person object');
+  }
+}
 
-const names: Readonly<string[]> = ['Max', 'Anna'];
-// names.push('Sara'); - error
+const pers = new Person();
+
+console.log(pers);
 
